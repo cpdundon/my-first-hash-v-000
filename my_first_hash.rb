2e68f9830e696443a28a4ee9991326123396f9b8
Chris Dundon <chris.dundon@gmail.com>
2018-04-02 22:30:56 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  lines = {"green" }
-  
+  lines = {"orange" => 11, "green" => 10, "red" => 15, "blue" => 5}
+  lines
 end
 
 
